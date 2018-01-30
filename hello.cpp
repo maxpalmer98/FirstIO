df236695fdf48fd24cda71a6680396e824e2f8cc
Maxwell A Palmer <mpalmer16@mail.csuchico.edu>
2018-01-29 17:20:43 -0800

    Changes the greeting

--- hello.cpp
+++ hello.cpp
@@ -1,9 +1,10 @@
+// Author: Maxwell Palmer 
 #include <iostream>
 #include <string>
 
 int main() {
     std::string input;
-    std::cout << "What's your first name?" << std::endl;
+    std::cout << "Hi, my name is Maxwell Palmer" << std::endl;
     std::cin >> input;
     std::cout << "Hello, " << input << std::endl;
     return 0;
